7a61bf65479a9ed68eeb3663b3094a3ac4e288d3
narusorn rojrattanatrai <41671821+narusornproject@users.noreply.github.com>
2019-02-19 23:26:55 +0700

    Fix problem function Fill zero

--- preprocessing.py
+++ preprocessing.py
@@ -1,82 +1,117 @@
-import preprocessing as pre
-import numpy
-from sklearn.model_selection import KFold
-import json
-import algorithm_ml as al
-import reduce_feature as rf
-# from tabulate import tabulate
+import deepcut
+import gensim
+import pymongo
+from gensim.models import Word2Vec
+from pythainlp.corpus import stopwords
+import pandas as pd
+import re
+import string
 
 
-def createObj(doc, similarity, value, type_true, type_pre, ck):
-    predict = {
-        'doc': doc,
-        'similarity': similarity,
-        'value': value,
-        'type_true': type_true,
-        'type_pre': type_pre,
-        'ck': ck
-    }
-    return predict
+def GetNews():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client['News']
+    col = db['DNews']
+    dataFrame = pd.DataFrame(list(col.find({'$or': [{'CategoryThai': 'อาชญากรรม'},
+                                                    {'CategoryThai': 'การเมือง'},
+                                                    {'CategoryThai': 'กีฬา'},
+                                                    {'CategoryThai': 'บันเทิง'},
+                                                    {'CategoryThai': 'การศึกษา'},
+                                                    {'CategoryThai': 'ต่างประเทศ'}]},
+                                           {'Header': True, 'CategoryThai': True, '_id': False}).limit(100)))
+    dataList = list(col.find({'$or': [{'CategoryThai': 'อาชญากรรม'},
+                                      {'CategoryThai': 'การเมือง'},
+                                      {'CategoryThai': 'กีฬา'},
+                                      {'CategoryThai': 'บันเทิง'},
+                                      {'CategoryThai': 'การศึกษา'},
+                                      {'CategoryThai': 'ต่างประเทศ'}]},
+                             {'Header': True, 'CategoryThai': True, '_id': False}).limit(100))
+    client.close()
+    return dataFrame, dataList
 
 
-def train_test(corpus, bagWord, train, test, type):
-    acc = 0
-    for x in test:
-        maxValue = 0
-        predict = createObj('', '', '', '', '', '')
-        for y in train:
-            if type == 'cosine':
-                try: cosine = al.Cosine(sims[pre.Fill(x, len(corpus))], sims[pre.Fill(y, len(corpus))])
-                except: cosine = 0
-                if cosine > maxValue:
-                    maxValue = cosine
-                    predict = createObj(corpus.index(x), type, cosine,
-                                        dl[corpus.index(y)]['CategoryThai'],
-                                        dl[corpus.index(x)]['CategoryThai'],
-                                        dl[corpus.index(y)]['CategoryThai'] == dl[corpus.index(x)]['CategoryThai'])
+def CleanText(str):
+    """ Remove number """
+    clr_str = re.sub(r'\d+', '', str).strip().strip("' '")
 
-            else:
-                try: jaccard = al.jaccard(set(x), set(y))
-                except: jaccard = 0
-                if jaccard > maxValue:
-                    maxValue = jaccard
-                    predict = createObj(bagWord.index(x), type, jaccard,
-                                        dl[bagWord.index(y)]['CategoryThai'],
-                                        dl[bagWord.index(x)]['CategoryThai'],
-                                        dl[bagWord.index(y)]['CategoryThai'] == dl[bagWord.index(x)]['CategoryThai'])
+    """ Remove symbols """
+    exclude = set(string.punctuation)
+    clr_str1 = ''.join(ch for ch in clr_str if ch not in exclude)
 
-        print(predict)
-        data.append(predict)
-        if predict['ck']:
-            acc += 1
-    accuracy_set = str((acc / len(test)) * 100)
-    print('Accuracy(%): ', accuracy_set)
+    """ Remove whitespaces """
+    exclude = set(string.whitespace)
+    clr_str2 = ''.join(ch for ch in clr_str1 if ch not in exclude)
+    return clr_str2
 
 
-if __name__ == '__main__':
-    data = []
-    df, dl = pre.GetNews()
-    bagWord = pre.createBag(df)
-    dictionary, tf_idf, corpus, sims = pre.createTFIDF(bagWord)
+def CutStopWord(strArray):
+    """ Remove stop words """
+    stop_words = set(stopwords.words('thai'))
+    result = [i for i in strArray if not i in stop_words]
+    return result
 
-    """ Process reduce feature"""
-    # min_count = 1
-    # index_reduce, vocab_tf = rf.reduceFeature(dictionary, corpus, min_count)
-    # del dictionary, tf_idf, corpus, sims
-    # bagWord_reduce = rf.wordReduce(bagWord, index_reduce)
-    # dictionary, tf_idf, corpus, sims = pre.createTFIDF(bagWord_reduce)
 
-    x = numpy.array(corpus)
-    y = numpy.array(bagWord)
-    kf = KFold(n_splits=10)
-    kf.get_n_splits(x)
+def createBag(dataFrame):
+    bagWord = []
+    for x in dataFrame['Header']:
+        new_str = CleanText(x)
+        """ Cut words """
+        bagWord.append(CutStopWord(deepcut.tokenize(new_str)))
+    return bagWord
 
-    for train_index, test_index in kf.split(x):
-        print("TRAIN:", train_index, "TEST:", test_index)
-        X_train, X_test = x[train_index], x[test_index]
-        y_train, y_test = y[train_index], y[test_index]
-        train_test(corpus, bagWord, X_train, X_test, 'cosine')
-        train_test(corpus, bagWord, y_train, y_test, 'jaccard')
 
-    with open('data.json', 'a+', encoding='utf-8') as outfile:
-        json.dump(data, outfile, ensure_ascii=False)
+def createModel(bagWord):
+    model = Word2Vec(bagWord, min_count=1)
+    return model
+
+
+def createTFIDF(bagWord):
+    """ 1 """
+    dictionary = gensim.corpora.Dictionary(bagWord)
+
+    # print(dictionary)
+    # print("Number of words in dictionary:", len(dictionary))
+
+    # for i in range(len(dictionary)):
+    #     print(i, dictionary[i])
+
+    corpus = [dictionary.doc2bow(gen_doc) for gen_doc in bagWord]
+    # print(corpus)
+
+    tf_idf = gensim.models.TfidfModel(corpus)
+    # print(tf_idf)
+
+    # similarities gensim
+    sims = gensim.similarities.Similarity('', tf_idf[corpus],
+                                           num_features=len(dictionary))
+
+    return dictionary, tf_idf, corpus, sims
+
+
+def Query(str, dictionary, tf_idf):
+    query_doc = [w for w in CutStopWord(
+        deepcut.tokenize(CleanText(str)))]
+    print(query_doc)
+    query_doc_bow = dictionary.doc2bow(query_doc)
+    print(query_doc_bow)
+    query_doc_tf_idf = tf_idf[query_doc_bow]
+    print(query_doc_tf_idf)
+    return query_doc_tf_idf
+
+
+def value_sort(t):
+    return t[0]
+
+
+def Fill(doc, len_dictionary):
+    result, last = [], 0.0
+    for d in doc:
+        while last < d[0]:
+            result.append((last, 0))
+            last += 1
+        result.append(d)
+        last = d[0] + 1
+    if len(result) != len_dictionary:
+        for a in range(len(result), len_dictionary):
+            result.append((a, 0))
+    return result
